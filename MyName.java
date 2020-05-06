ba4a7973a960c7d8810e855751bdf067f747e265
ylakshmareddy <ylreddy2015@gmail.com>
2020-05-06 19:08:14 +0530

    Added my favorite food

--- MyName.java
+++ MyName.java
@@ -5,5 +5,6 @@ class MyName
 		System.out.println("My name is Lakshma Reddy.");
 		System.out.println("I'm from Guntur.");
 		System.out.println("I like to write code.");
+		System.out.println("I love Biryani.");
 	}
 }
