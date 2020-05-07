95e372588c6bff25b519d2394f82e84fbc53c750
ylakshmareddy <56040705+ylakshmareddy@users.noreply.github.com>
2020-05-07 08:25:48 +0530

    Update MyName.java

--- MyName.java
+++ MyName.java
@@ -6,8 +6,9 @@ class MyName
 		System.out.println("I'm from Guntur.");
 		System.out.println("I like to write code.");
 		System.out.println("I like beaches.");
-		System.out.println("I want to visit goa.");
+		System.out.println("I want to visit Goa.");
 		System.out.println("I like winters.");
+		System.out.println("I visited Chennai twice.");
 
 	}
 }
