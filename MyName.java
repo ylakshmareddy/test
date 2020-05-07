215e58764d49a6c46a0283d5839218fe947e6ee7
ylakshmareddy <ylreddy2015@gmail.com>
2020-05-07 11:51:43 +0530

    Removed few things

--- MyName.java
+++ MyName.java
@@ -6,12 +6,8 @@ class MyName
 		System.out.println("I'm from Guntur.");
 		System.out.println("I like to write code.");
 		System.out.println("I like beaches.");
-<<<<<<< HEAD
 		System.out.println("I want to visit Goa.");
 		System.out.println("I like winters.");
 		System.out.println("I visited Chennai twice.");
-
-=======
->>>>>>> parent of 58391d4... Added tourist destination
 	}
 }
