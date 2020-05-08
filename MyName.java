3ff7f21473224675325660f2df1b0bec103e469f
ylakshmareddy <ylreddy2015@gmail.com>
2020-05-08 14:50:24 +0530

    modularity

--- MyName.java
+++ MyName.java
@@ -2,10 +2,17 @@ class MyName
 {
 	public static void main(String[] args)
 	{
+		printMyDetails();
+		
+	}
+	public static void printMyDetails()
+	{
 		System.out.println("My name is Lakshma Reddy.");
 		System.out.println("I'm from Guntur.");
 		System.out.println("I like to write code.");
 		System.out.println("I like beaches.");
 		System.out.println("I want to visit goa.");
+		
 	}
+
 }
